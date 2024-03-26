baf2bafbd69cc4f0871d83945573c0b7e8144535
shubham4f <shubham4f@gmail.com>
2024-03-26 16:50:55 +0530

    Range on/Off, multi-bill delete

---
+++ server/routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./controllers');
+
+
+//Pricing
+router.get('/price', controller.viewPrice);
+router.post('/price', controller.createPrice);
+
+router.get('/editprice/:id', controller.editPriceView);
+router.post('/editprice', controller.editPrice);
+
+
+//customer
+router.get('/', controller.home);
+router.post('/', controller.homeDate);
+router.get('/:m/:o/:d',controller.allToggle);
+router.get('/:id/:m/:o/:d', controller.homeToggle);
+router.post('/rangeToggle/:id', controller.rangeToggle, controller.editCustomerForm );
+
+router.get('/addcustomer', controller.createCustomerForm);
+router.post('/addcustomer', controller.createCustomer);
+
+router.get('/editcustomer/:id',controller.editCustomerForm);
+router.post('/editcustomer',controller.editCustomer);
+router.get('/deletecustomer/:id',controller.deleteCustomer);
+
+// bill
+
+router.get('/bill', controller.bill);
+router.post('/bill', controller.bill);
+router.get('/bill/:id', controller.genrateBill);
+router.get('/deletebill/:id', controller.deleteBill);
+router.get('/genrateBill/:id',controller.instantBill);
+router.post('/deletemultibill', controller.deleteMultiBill, controller.bill);
+
+
+
+
+
+
+
+module.exports = router;
